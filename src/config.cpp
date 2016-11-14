9a2bc4be4b37b92691687b1f77d0d83eb8126c64
Dave <dave@d53dev.net>
2016-11-14 22:07:42 +0100

    Refactored Config class and added static initializer with default values.

---
+++ src/config.cpp
@@ -0,0 +1,22 @@
+//
+// Created by dotdi on 14.11.16.
+//
+
+#include "config.hpp"
+
+namespace CSAOpt {
+
+    Config::ConfigMap Config::config {
+            //initializing defaults
+            {"logger.name",            "console"},
+            {"logger.path",            "~/.csaopt/"},
+            {"logger.filename",        "csaopt.log"},
+            {"logger.verbose",         "false"},
+            {"ansible.hosts",          "~/.csaopt/hosts"},
+            {"optimization.maxcycles", ""},
+            {"optimization.classes",   ""},
+            {"optimization.targets",   ""},
+            {"optimization.type",      ""}
+    };
+}
+
--- src/config.hpp
+++ src/config.hpp
@@ -2,27 +2,72 @@
 
 #include <istream>
 #include <chrono>
+#include <map>
+#include <sstream>
 #include <string>
+#include <fstream>
+#include <spdlog/spdlog.h>
 
-namespace CSAOpt{
+namespace CSAOpt {
     class Config {
     public:
-        Config();
-        ~Config();
-        Config& operator=(const Config&);
-        Config(const Config&);
-        Config parsePath(std::string path);
+        static void parse(std::string path) {
+            auto logger = spdlog::get(Config::loggerName());
+            //TODO: do some validation
+            std::ifstream file(path);
+            std::string line;
+            while (std::getline(file, line)) {
+                if (line.find("=") == std::string::npos) {
+                    logger->warn("Config::parse: Line '{}' malformed, skipping.", line);
+                    continue;
+                }
+                config[trim(line.substr(0, line.find('=')))] = trim(line.substr(line.find('=') + 1));
+            }
+        };
 
-        static bool verboseLogging(){ return true; }
-        static bool fileLogging(){ return std::string{loggerName()} == "dailyLogger"; }
-        static const char *loggerName(){ return loggerNameStr; }
-        static std::string hostFilePath(){ return "./ansible/hosts";}
+        static bool verboseLogging() {
+            return config["logger.verbose"] == "true";
+        }
 
+        static bool fileLogging() {
+            return config["logger.name"] == "file";
+        }
 
-        enum logLevel {info, warning, error, critical, debug};
+        static std::string loggerFileName() {
+            return config["logger.filename"];
+        }
+
+        static std::string loggerName() {
+            return config["logger.name"];
+        }
+
+        static std::string hostFilePath() {
+            return config["ansible.hosts"];
+        }
+
+        static std::string getAll() {
+            std::stringstream ss;
+            for (auto &&kvp : config) {
+                ss << kvp.first << "=" << std::endl;
+            }
+            return ss.str();
+        }
+
     private:
-        static bool verbose;
-        static constexpr const char* loggerNameStr = "console";
+        typedef std::map<std::string, std::string> ConfigMap;
+
+        Config() { };
+
+        ~Config() = default;
+
+        static ConfigMap config;
+
+        static inline std::string trim(const std::string &s) {
+            auto whitespaceFront = std::find_if_not(s.begin(), s.end(), [](int c) { return std::isspace(c); });
+            return std::string(whitespaceFront,
+                               std::find_if_not(s.rbegin(), std::string::const_reverse_iterator(whitespaceFront),
+                                                [](int c) { return std::isspace(c); }).base());
+        }
     };
 }
 
