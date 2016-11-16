7bbf680ab8d61ebe417fbec6695aafd0ab35710b
Dave <dave@d53dev.net>
2016-11-16 23:58:01 +0100

    Added runCmd for cerr and DRYed up a bit,

--- src/SysTools.h
+++ src/SysTools.h
@@ -14,21 +14,38 @@ typedef std::istreambuf_iterator<char> pstrm_iter;
 
 class SysTools {
 public:
-    static std::string runCmd(const std::string cmd) {
-        redi::ipstream proc(cmd);
+    static std::string runCmd(std::string const &cmd) {
+        return runCmd(cmd, redi::pstream::pstdout);
+    }
+
+    static std::string runCmdGetCerr(std::string const &cmd) {
+        return runCmd(cmd, redi::pstream::pstderr);
+    }
+
+    static std::string runCmd(std::string const &cmd, int &retCode) {
+        return runCmd(cmd, retCode, redi::pstream::pstdout);
+    }
+
+    static std::string runCmdGetCerr(std::string const &cmd, int &retCode) {
+        return runCmd(cmd, retCode, redi::pstreams::pstderr);
+    }
+
+private:
+    static std::string runCmd(std::string const &cmd, const redi::pstream::pmode stream) {
+        redi::ipstream proc(cmd, stream);
 
         std::string output(pstrm_iter(proc.rdbuf()), pstrm_iter());
 
         return trim(output);
     }
 
-    static std::string runCmd(const std::string cmd, int &retCode) {
-        redi::ipstream proc(cmd);
+    static std::string runCmd(std::string const &cmd, int &retCode, const redi::pstream::pmode stream) {
+        redi::ipstream proc(cmd, stream);
 
         std::string output(pstrm_iter(proc.rdbuf()), pstrm_iter());
 
         proc.close();
-        if (proc.rdbuf()->exited()){
+        if (proc.rdbuf()->exited()) {
             int encodedRetCode = proc.rdbuf()->status();
             retCode = WEXITSTATUS(encodedRetCode);
         }
