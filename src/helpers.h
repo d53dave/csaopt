f4dbed3e92bc7d7d9ea8a984ce46445a2bd8b03e
Dave <dave@d53dev.net>
2016-11-16 15:42:48 +0100

    Pulled out some helper functions into header

---
+++ src/helpers.h
@@ -0,0 +1,33 @@
+//
+// Created by dotdi on 15.11.16.
+//
+
+#pragma once
+
+static inline std::string trim(const std::string &s) {
+    auto whitespaceFront = std::find_if_not(s.begin(), s.end(), [](int c) { return std::isspace(c); });
+    return std::string(whitespaceFront,
+                       std::find_if_not(s.rbegin(), std::string::const_reverse_iterator(whitespaceFront),
+                                        [](int c) { return std::isspace(c); }).base());
+}
+
+
+
+static inline std::string string_format(const std::string fmt_str, ...) {
+    int final_n, n = ((int) fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+    std::string str;
+    std::unique_ptr<char[]> formatted;
+    va_list ap;
+    while (1) {
+        formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
+        strcpy(&formatted[0], fmt_str.c_str());
+        va_start(ap, fmt_str);
+        final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
+        va_end(ap);
+        if (final_n < 0 || final_n >= n)
+            n += abs(final_n - n + 1);
+        else
+            break;
+    }
+    return std::string(formatted.get());
+}
