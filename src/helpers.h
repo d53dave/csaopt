0b319f146297461cd783dc8c420b41c3127ecde4
Dave <dave@d53dev.net>
2016-11-16 21:26:26 +0100

    Added path/string handling functions

--- src/helpers.h
+++ src/helpers.h
@@ -4,14 +4,25 @@
 
 #pragma once
 
-static inline std::string trim(const std::string &s) {
+static inline std::string getFilenameFromPath(std::string const &path) {
+    return path.substr(path.find_last_of("/\\") + 1);
+}
+
+static inline std::string removeFileExtension(std::string const &filename) {
+    size_t idx = filename.find_last_of('.');
+    return idx > 0 && idx != filename.npos ? filename.substr(0, idx) : filename;
+}
+
+static inline std::string getFilenameWithoutExtension(std::string const &path) {
+    return removeFileExtension(getFilenameFromPath(path));
+}
+
+static inline std::string trim(std::string const &s) {
     auto whitespaceFront = std::find_if_not(s.begin(), s.end(), [](int c) { return std::isspace(c); });
     return std::string(whitespaceFront,
                        std::find_if_not(s.rbegin(), std::string::const_reverse_iterator(whitespaceFront),
                                         [](int c) { return std::isspace(c); }).base());
 }
-
-
 
 static inline std::string string_format(const std::string fmt_str, ...) {
     int final_n, n = ((int) fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
