419f546cc5201926e798923adbe9ab5e313978af
Dave <dave@d53dev.net>
2016-11-18 01:24:12 +0100

    Added function for path normalization

--- src/helpers.h
+++ src/helpers.h
@@ -6,6 +6,8 @@
 
 #include <cstdarg>
 #include <sys/stat.h>
+#include <errno.h>
+#include <wordexp.h>
 
 static inline std::string getFilenameFromPath(std::string const &path) {
     return path.substr(path.find_last_of("/\\") + 1);
@@ -30,4 +32,22 @@ static inline std::string trim(std::string const &s) {
 static inline bool file_exists(std::string const &name) {
     struct stat buffer;
     return (stat(name.c_str(), &buffer) == 0);
+}
+
+static inline std::string relativePathToAbsolute(std::string const &path) {
+    wordexp_t exp_result;
+    wordexp(path.c_str(), &exp_result, 0);
+
+    if (exp_result.we_wordc > 0) {
+        std::string expandedPath = exp_result.we_wordv[0];
+
+        char *real_path = realpath(expandedPath.c_str(), NULL);
+        if(real_path == nullptr){
+            return expandedPath;
+        }
+        std::string filename = std::string{real_path};
+        free(real_path);
+        return filename;
+    }
+    return "";
 }
