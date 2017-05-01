aa96e7bf6a20dfd79958286a1ab18e1f9271b06a
Dave <dave@d53dev.net>
2017-05-01 20:41:22 +0200

    Added some more method stubs

--- src/MessageQueueClient.h
+++ src/MessageQueueClient.h
@@ -8,11 +8,17 @@
 #include <map>
 #include <vector>
 #include <Target.h>
+#include "OptimizationJob.h"
 
 
 namespace CSAOpt {
+
+    typedef std::string JobId;
+
     class MessageQueueClient {
 
+        JobId submit(const OptimizationJob && job);
+        void collect(const JobId && id);
         bool didFinish(std::string jobId);
 
     private:
