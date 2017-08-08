89c88feb5c14346c0ef63df7889375f4e7f002f4
Dave <dave@d53dev.net>
2017-08-08 22:50:07 +0200

    Added __str__ method for BuildResult

--- app/modelcompiler/__init__.py
+++ app/modelcompiler/__init__.py
@@ -12,3 +12,8 @@ class BuildResult:
 
     def failed(self) -> bool:
         return self.errors is not None and len(self.errors) > 0
+
+    def __str__(self) -> str:
+        failed_or_success = 'success' if not self.failed() else 'failed'
+        results = 'artifacts={}'.format(self.artifacts) if not self.failed() else 'errors={}'.format(self.errors)
+        return 'BuildResult [{}]: {}'.format(failed_or_success, results)
