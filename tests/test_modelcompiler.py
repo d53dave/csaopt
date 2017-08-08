46da972ab20341d24de4e47047b45bb22151898b
Dave <dave@d53dev.net>
2017-08-08 08:22:03 +0200

    Steps towards getting modelcompiler test to run

--- tests/test_modelcompiler.py
+++ tests/test_modelcompiler.py
@@ -1,14 +1,26 @@
 import pytest
 import context
 
+from collections import defaultdict
+
+
 @pytest.fixture
 def model_compiler():
     model_proj_path = ''
-    conf = ''
-    internal_conf = ''
+    conf = defaultdict(str)
+
+    conf['build.exec_paths'] = []
+
+    internal_conf = defaultdict(str)
+
+    internal_conf['build.exec_names'] = ['cmake', 'make']
+    internal_conf['build.required_artifacts'] = 'model.o'
+
     return context.ModelCompiler(model_proj_path, conf, internal_conf)
 
 
 def test_build(model_compiler):
     print(dir(model_compiler))
+
+    model_compiler.build()
     assert 1 is 2
