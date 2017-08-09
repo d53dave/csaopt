a153c19abcbcaa816fcbc37e814c22cdd44de806
Dave <dave@d53dev.net>
2017-08-09 11:01:44 +0200

    pytest now injects configs, not the modelcompiler

--- tests/test_modelcompiler.py
+++ tests/test_modelcompiler.py
@@ -5,24 +5,27 @@ from collections import defaultdict
 
 
 @pytest.fixture
-def model_compiler():
-    model_proj_path = ''
+def conf():
     conf = defaultdict(str)
-
     conf['build.exec_paths'] = []
+    return conf
 
+
+@pytest.fixture
+def internal_conf():
     internal_conf = defaultdict(str)
 
     internal_conf['build.exec_names'] = ['cmake', 'make']
     internal_conf['build.required_artifacts'] = 'model.o'
     internal_conf['build.timeouts.cmake'] = 30
     internal_conf['build.timeouts.make'] = 30
+    return internal_conf
 
-    return context.ModelCompiler(model_proj_path, conf, internal_conf)
 
+def test_build(conf, internal_conf):
+    model_proj_path = ''
+    model_compiler = context.ModelCompiler(model_proj_path, conf, internal_conf)
 
-def test_build(model_compiler):
     result = model_compiler.build()
 
     assert result.failed()
-    
