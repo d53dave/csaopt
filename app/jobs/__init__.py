cfb4dcf2b44bb0113527c46652e6f94e71bd9e69
Dave <dave@d53dev.net>
2017-08-14 09:11:04 +0200

    Added jobmanager and job class

---
+++ app/jobs/__init__.py
@@ -0,0 +1,19 @@
+import uuid
+import zstdandard as zstd
+import ujson
+
+from Typing import Dict
+from Typing import object
+
+__all__ = ['compiler']
+
+
+class Job():
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.expected_responses = []
+
+    def _uncompress_payload(self, compressed: bytes) -> Dict[str, object]:
+        dctx = zstd.ZstdDecompressor()
+        decompressed = dctx.decompress(compressed)
+        return ujson.reads(decompressed)
---
+++ app/jobs/jobmanager.py
@@ -0,0 +1,5 @@
+
+class JobManager():
+
+    def __init__(self):
+        pass
