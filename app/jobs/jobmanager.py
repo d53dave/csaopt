34a576417f42df9373df3a873b2e039bbf8aa81e
Dave <dave@d53dev.net>
2017-08-26 23:19:16 +0200

    Added preliminary docstring to jobmanager

--- app/jobs/jobmanager.py
+++ app/jobs/jobmanager.py
@@ -1,5 +1,16 @@
+from . import Job
+from . import SubmissionResult
+
 
 class JobManager():
+    """This class handles submission, tracking and retrieval of optimization jobs.
 
+    This abstracts away the detailed communication with the message queue through
+    the msgqueue client class.
+    
+    """
     def __init__(self):
+        pass
+
+    def submit(job: Job) -> SubmissionResult:
         pass
