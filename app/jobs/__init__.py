19eac308085446577d855f8aff8363733b344f9d
Dave <dave@d53dev.net>
2017-08-26 23:20:30 +0200

    Added SubmissionResult class and Job.finished method

--- app/jobs/__init__.py
+++ app/jobs/__init__.py
@@ -12,10 +12,22 @@ class Job():
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.expected_responses = []
+        self.payload = None
 
-    
+    def finished(self):
+        return self.payload is not None
 
     def _uncompress_payload(self, compressed: bytes) -> Dict[str, object]:
+        """The message payload is a zstd compressed json string. Using zstandard and ujson to process"""
         dctx = zstd.ZstdDecompressor()
         decompressed = dctx.decompress(compressed)
         return ujson.loads(decompressed)
+
+
+class SubmissionResult():
+    def __init__(self, jobId, error):
+        self.error = error
+        self.jobId = jobId
+
+    def failed(self) -> bool:
+        return self.error is not None
