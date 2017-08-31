d892d4795f302a1c52d54b270a2540363d03acb4
Dave <dave@d53dev.net>
2017-08-31 23:02:43 +0200

    Introduced instancemanager interface

---
+++ app/instancemanager/instancemanager.py
@@ -0,0 +1,37 @@
+import requests
+import random
+import string
+import abc
+
+
+def _get_own_ip():
+    return requests.get('https://api.ipify.org/').text
+
+
+def _get_random_string(length=8):
+    return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(length)])
+
+
+class InstanceManager():
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def _provision_instances(count=2, **kwargs):
+        """Start and configure instances"""
+
+    @abs.abstractmethod
+    def _get_running_instances(self):
+        """Returns the currently managed instances"""
+
+    @abs.abstractmethod
+    def _terminate_instances(self):
+        """Terminate managed instances"""
+
+    @abs.abstractmethod
+    def __enter__(self):
+        """InstanceManager is a ContextManager"""
+
+    @abs.abstractmethod
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Cleanup resources on exit"""
