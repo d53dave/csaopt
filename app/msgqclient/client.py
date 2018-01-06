d6d7dbaf1faeb3138e11544f639ecf62717abc94
Dave <dave@d53dev.net>
2018-01-06 23:54:46 +0100

    Kafka client implementation

--- app/msgqclient/client.py
+++ app/msgqclient/client.py
@@ -1,39 +1,81 @@
-import zmq
-import asyncio
-from tornado.ioloop import IOLoop
-import capnp
-import os
+import logging
+import msgpack
+
+from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
 from app.jobs import Job
 
-CWD = os.path.dirname(__file__)
-tidings_capnp = capnp.load(os.path.join(CWD, 'capnp/src/tidings.capnp'))
-plumbing_capnp = capnp.load(os.path.join(CWD, 'capnp/src/plumbing.capnp'))
+log = logging.getLogger(__name__)
 
 
 class QueueClient:
-    def __init__(self, ioloop, host, port):
-        self.ctx = zmq.asyncio.Context()
-        self.socket = self.ctx.socket(zmq.REQ)
-        self.socket.connect('{}:{}'.format(host, port))
-        ioloop.spawn_callback(self._get_stats_loop)
+
+    @classmethod
+    async def create(cls, ioloop, conf):
+        self = QueueClient()
+        self.conf = conf
+        print('Starting client with config: ', conf)
+        self.consumer = AIOKafkaConsumer(
+            *conf['kafka.consumer_topics'],
+            loop=ioloop, bootstrap_servers=conf['kafka.servers'],
+            group_id=conf['kafka.consumer_group'],
+            key_deserializer=QueueClient._KeyDeserializer(),
+            value_deserializer=QueueClient._ValueDeserializer())
+
+        self.producer = AIOKafkaProducer(
+            loop=ioloop, bootstrap_servers=conf['kafka.servers'],
+            key_serializer=QueueClient._KeySerializer(),
+            value_serializer=QueueClient._ValueSerializer())
+
+        await self.consumer.start()
+        await self.producer.start()
+
+        return self
+
+    def __init__(self):
         self.submitted = {}
 
-    def _process_submit_answer(self, resp):
-        message = plumbing_capnp.Plumbing.from_bytes_packed(resp[0])
-        self.submitted[message.id] = message
+    async def _consume(self):
+        try:
+            # Consume messages
+            async for msg in self.consumer:
+                log.debug("consumed: ", msg.topic, msg.partition, msg.offset,
+                          msg.key, msg.value, msg.timestamp)
+                if(msg.topic == 'csaopt.data.in.t'):
+                    pass
+                if(msg.topic == 'csaopt.stats.in.t'):
+                    pass
+                print(msg)
+        finally:
+            # Will leave consumer group; perform autocommit if enabled.
+            await self.consumer.stop()
+            await self.producer.stop()
 
-    async def _send_request(self, message):
-        await self.socket.send_multipart(message)
-        return await self.socket.recv_multipart()
-
-    async def _get_stats_loop(self):
-        pass
+    async def _send_one(self, topic, key=None, value=None):
+        try:
+            # Produce message
+            await self.producer.send(topic, key=key, value=value)
+        finally:
+            # Wait for all pending messages to be delivered or expire.
+            pass
 
     async def submit_job(self, job: Job):
-        message = tidings_capnp.Tiding.new_message()  # TODO finish this
-        resp = await self._send_request(message)
-        return self._process_submit_answer(resp)
+        self.submitted[job.id] = job
 
     def get_results(id):
         pass
 
+    class _KeySerializer:
+        def __call__(self, key: str) -> bytes:
+            return key.encode(encoding='utf_8') if key is not None else b''
+
+    class _ValueSerializer:
+        def __call__(self, value: object) -> bytes:
+            return msgpack.packb(value, use_bin_type=True)
+
+    class _KeyDeserializer:
+        def __call__(self, keyBytes: bytes) -> str:
+            return keyBytes.decode(encoding='utf-8') if bytes is not None else ''
+
+    class _ValueDeserializer:
+        def __call__(self, valueBytes: bytes) -> object:
+            return msgpack.unpackb(valueBytes)
