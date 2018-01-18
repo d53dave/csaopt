bbce7b7b4c168312b67c5471d00422e72b302646
Dave <dave@d53dev.net>
2018-01-18 23:18:37 +0100

    Refactored and added heartbeat handling

--- app/msgqclient/client.py
+++ app/msgqclient/client.py
@@ -1,8 +1,13 @@
 import logging
 import msgpack
+import asyncio
 
+from typing import Dict, Any
 from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
+from asyncio.selector_events import BaseSelectorEventLoop as EventLoop
+
 from app.jobs import Job
+from . import Worker
 
 log = logging.getLogger(__name__)
 
@@ -10,29 +15,47 @@ log = logging.getLogger(__name__)
 class QueueClient:
 
     @classmethod
-    async def create(cls, ioloop, conf):
-        self = QueueClient()
-        self.conf = conf
-        print('Starting client with config: ', conf)
-        self.consumer = AIOKafkaConsumer(
-            *conf['kafka.consumer_topics'],
+    async def create(cls, ioloop: EventLoop, conf: Dict[str, Any]):
+        management_recv_topic = conf['kafka.topics.management_recv']
+        data_recv_topic = conf['kafka.topics.management_recv']
+
+        consumer = AIOKafkaConsumer(
+            [management_recv_topic, data_recv_topic],
             loop=ioloop, bootstrap_servers=conf['kafka.servers'],
             group_id=conf['kafka.consumer_group'],
             key_deserializer=QueueClient._KeyDeserializer(),
             value_deserializer=QueueClient._ValueDeserializer())
 
-        self.producer = AIOKafkaProducer(
+        producer = AIOKafkaProducer(
             loop=ioloop, bootstrap_servers=conf['kafka.servers'],
             key_serializer=QueueClient._KeySerializer(),
             value_serializer=QueueClient._ValueSerializer())
 
+        self = QueueClient(
+            management_recv_topic,
+            data_recv_topic,
+            consumer,
+            producer
+        )
+
         await self.consumer.start()
         await self.producer.start()
 
+        worker_timeout = conf['worker.timeout_seconds']
+        asyncio.Task(self._update_worker_timeout(worker_timeout))
+
         return self
 
-    def __init__(self):
-        self.submitted = {}
+    def __init__(self,
+                 management_recv_topic: str,
+                 data_recv_topic: str,
+                 consumer: AIOKafkaConsumer,
+                 producer: AIOKafkaProducer) -> None:
+        self.workers: Dict[str, Worker] = {}
+        self.management_recv_topic: str = management_recv_topic
+        self.data_recv_topic: str = data_recv_topic
+        self.consumer = consumer
+        self.producer = producer
 
     async def _consume(self):
         try:
@@ -40,15 +63,29 @@ class QueueClient:
             async for msg in self.consumer:
                 log.debug("consumed: ", msg.topic, msg.partition, msg.offset,
                           msg.key, msg.value, msg.timestamp)
-                if(msg.topic == 'csaopt.data.in.t'):
+                if msg.topic == self.management_recv_topic:
                     pass
-                if(msg.topic == 'csaopt.stats.in.t'):
+                if msg.topic == self.data_recv_topic:
                     pass
                 print(msg)
         finally:
             # Will leave consumer group; perform autocommit if enabled.
             await self.consumer.stop()
             await self.producer.stop()
+
+    def _handle_worker_heartbeat(self, worker_id: str, hb_timestamp_utc: int):
+        if worker_id in self.workers:
+            self.workers[worker_id].update_heartbeat(hb_timestamp_utc)
+
+    async def _update_worker_timeout(self, worker_timeout_seconds):
+        while True:
+            await asyncio.sleep(1)
+            new_workers: Dict[str, Worker] = {}
+            for worker in self.workers:
+                if worker.alive(worker_timeout_seconds):
+                    new_workers[worker.id] = worker
+                else:
+                    log.warn('Worker [{}] timed out.'.format(worker.id))
 
     async def _send_one(self, topic, key=None, value=None):
         try:
@@ -59,7 +96,8 @@ class QueueClient:
             pass
 
     async def submit_job(self, job: Job):
-        self.submitted[job.id] = job
+        # self.submitted[job.id] = job
+        pass
 
     def get_results(id):
         pass
