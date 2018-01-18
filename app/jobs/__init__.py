81fe4753089d98e0eadfea72a3b5fac9e6eb148b
Dave <dave@d53dev.net>
2018-01-18 23:18:52 +0100

    Removed obsolete code

--- app/jobs/__init__.py
+++ app/jobs/__init__.py
@@ -1,34 +1,15 @@
 import uuid
-import zstd
-import ujson
 
-from typing import Dict
+from ..model import Model
 
 __all__ = ['jobmanager']
 
 
 class Job():
-    def __init__(self):
+    def __init__(self, model: Model) -> None:
         self.id = str(uuid.uuid4())
-        self.expected_responses = []
-        self.payload = None
-        self.optimization_name = None
-        self.state_name = None
+        self.model = Model
+        self.results = []
 
     def finished(self):
-        return self.payload is not None
-
-    def _uncompress_payload(self, compressed: bytes) -> Dict[str, object]:
-        """The message payload is a zstd compressed json string. Using zstandard and ujson to process"""
-        dctx = zstd.ZstdDecompressor()
-        decompressed = dctx.decompress(compressed)
-        return ujson.loads(decompressed)
-
-
-class SubmissionResult():
-    def __init__(self, jobId, error):
-        self.error = error
-        self.jobId = jobId
-
-    def failed(self) -> bool:
-        return self.error is not None
+        return self.results is not None and len(self.results) > 0
