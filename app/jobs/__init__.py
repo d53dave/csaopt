f1de28c42b26ff2e616ad66e68e4cbd64ae948da
Dave <dave@d53dev.net>
2018-02-02 21:53:47 +0100

    Cleaned up Job class

--- app/jobs/__init__.py
+++ app/jobs/__init__.py
@@ -1,15 +1,16 @@
 import uuid
 
+from typing import Dict, List, Any
+
 from ..model import Model
 
 __all__ = ['jobmanager']
 
 
 class Job():
-    def __init__(self, model: Model) -> None:
+    def __init__(self, model: Model, opt_params: Dict[str, Any]) -> None:
         self.id = str(uuid.uuid4())
         self.model = Model
-        self.results = []
-
-    def finished(self):
-        return self.results is not None and len(self.results) > 0
+        self.results: List[List[Any]] = []
+        self.values: List[List[float]] = []
+        self.finished = False
