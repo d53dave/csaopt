e8a02a824431a866f682823bdfef9b791153bb8c
David Sere <dave@d53dev.net>
2018-05-07 10:47:27 +0200

    Fixed failing tests after moving AWS

--- tests/context.py
+++ tests/context.py
@@ -6,7 +6,7 @@ from distutils import dir_util
 
 sys.path.insert(0, os.path.abspath('.'))
 
-from app.aws.awstools import AWSTools
+from app.instancemanager.awstools import AWSTools
 from app.msgqclient import Worker
 from app.msgqclient.client import QueueClient
 from app.utils import get_own_ip
