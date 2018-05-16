20279bed1904b9e8556e4fb75cbffce35efe92b0
David Sere <dave@d53dev.net>
2018-05-16 22:21:47 +0200

    Added AWS userdata scripts

---
+++ app/internal/aws-userdata-kafka.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Start kafka:
+docker-compose -f docker-compose-single-broker.yml up
---
+++ app/internal/aws-userdata-worker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get Image
+docker pull d53dave/csaopt-worker:1.0.0
+
+# Set ENV variables for kafka
+
+# Run
+docker run -it 
