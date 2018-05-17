32a7d8425e60eae8181f5d34b8e9222d70fac6dd
David Sere <dave@d53dev.net>
2018-05-17 07:02:53 +0200

    Fixed EC2 client instantiation, added TODOs

--- app/instancemanager/awstools.py
+++ app/instancemanager/awstools.py
@@ -1,12 +1,13 @@
 import boto3
-from botocore.exceptions import ClientError
 import logging
+
+from botocore.exceptions import ClientError
+from typing import List, Dict, Any
 
 from ..instancemanager.instancemanager import InstanceManager
 from ..utils import get_own_ip
 
 logger = logging.getLogger()
-
 
 class AWSTools(InstanceManager):
     """The AWSTools class provides an abstraction over boto3 and EC2 for the use with CSAOpt
@@ -29,48 +30,72 @@ class AWSTools(InstanceManager):
         * AWS_SECRET_ACCESS_KEY
 
     """
+
+    AwsInstance = Dict[str, Any]
+
     def __init__(self, config, internal_conf):
+        if config['cloud.aws.region'] is not None:
+            self.region = config['cloud.aws.region'] 
+        else:
+            self.region = internal_conf['cloud.aws.region']
+        
         if config['aws.secret_key'] and config['aws.access_key']:
-            self.ec2 = boto3.client(
-                aws_access_key_id=config['aws.access_key'],
-                aws_secret_access_key=config['aws.secrey_key']
-            )
+            self.ec2_resource = boto3.resource('ec2', 
+                                                aws_access_key_id=config['aws.access_key'],
+                                                aws_secret_access_key=config['aws.secrey_key'], 
+                                                region_name=self.region)
+
+
+            
         else:
             # This will look for the env variables
-            self.ec2 = boto3.client('ec2')
+            self.ec2_resource=boto3.client('ec2')
 
-        self.region
-        self.instances = []
-        self.sec_group = None
-        self.private_key_path = None
-        self.worker_count = config['aws.worker_count']
-        self.separate_queue_instance = config['aws.separate_queue_instance']
+
+        self.ec2_client = ec2_resource.meta.client
+        self.instances: List[AwsInstance] = []
+        self.message_queue: AwsInstance = []
+        self.security_group_id: str = ''
+        self.worker_count: int = config['cloud.aws.worker_count']
 
     def _provision_instances(self, count=2, **kwargs):
         """Start and configure instances"""
 
-        imageId = kwargs.get('imageId', default='ami-1e299d7e')
+        # MessageQueue
+        imageId = kwargs.get('imageId', default=self.default_message_queue_ami)
+        instanceType = 'm4.large' # TODO pull from config
+        self.message_queue = self.ec2_client.create_instances(ImageId=imageId,
+                                                              MinCount=1,
+                                                              MaxCount=1,
+                                                              InstanceType=instanceType)
+
+        # Workers
+        imageId = kwargs.get('imageId', default=self.default_worker_ami)
+        # TODO pull from config
         instanceType = kwargs.get('instanceType', default='t2.micro')
 
-        self.instances = self.ec2.create_instances(
+        self.instances = self.ec2_client.create_instances(
                 ImageId=imageId,
                 MinCount=count,
                 MaxCount=count,
-                InstanceType=instanceType)
+                InstanceType=instanceType,
+                SecurityGroupIds=[self.security_group_id]))
 
     def _get_running_instances(self):
         """Returns the currently managed instances"""
+        # TODO: implement
 
     def _terminate_instances(self):
         """Terminate all instances managed by AWSTools"""
         instance_ids = [instance.id for instance in self.instances]
-        self.ec2.instances.filter(InstanceIds=instance_ids).stop()
         self.ec2.instances.filter(InstanceIds=instance_ids).terminate()
 
     def __enter__(self):
         """On enter, AWSTools prepares the AWS security group and spins up the required intances"""
         self._create_sec_group()
-        self._provision_instances()
+
+        # TODO: put all required parameters into kwargs
+        self._provision_instances(count=self.worker_count)
 
     def __exit__(self, exc_type, exc_value, traceback):
         """On exit, AWSTools terminates the started instances and removes security groups"""
@@ -104,7 +129,7 @@ class AWSTools(InstanceManager):
 
             self.security_group_id = response['GroupId']
 
-            data = self.ec2.authorize_security_group_ingress(
+            data = self.ec2_client.authorize_security_group_ingress(
                 GroupId=self.security_group_id,
                 IpPermissions=[  # TODO: 80 and 22 are not needed here. Rather, these should be the zmq ports
                     {'IpProtocol': 'tcp',
