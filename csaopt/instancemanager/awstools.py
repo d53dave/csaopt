3836990321dc106798f52fbe47863e551b0fd361
David Sere <dave@d53dev.net>
2018-05-20 21:51:07 +0200

    Fixed linter errors

--- csaopt/instancemanager/awstools.py
+++ csaopt/instancemanager/awstools.py
@@ -1,14 +1,17 @@
 import boto3
 import logging
 
+from multiprocessing import Process
 from botocore.exceptions import ClientError
 from typing import List, Dict, Any
 
 from . import Instance
 from ..instancemanager.instancemanager import InstanceManager
-from ..utils import get_own_ip
+from ..utils import get_own_ip, random_str
 
 logger = logging.getLogger()
+
+AwsInstance = Dict[str, Any]
 
 class AWSTools(InstanceManager):
     """The AWSTools class provides an abstraction over boto3 and EC2 for the use with CSAOpt
@@ -32,37 +35,38 @@ class AWSTools(InstanceManager):
 
     """
 
-    def __init__(self, config, internal_conf) -> None:
+    
+
+    def __init__(self, context, config, internal_conf) -> None:
         if config['cloud.aws.region'] is not None:
             self.region = config['cloud.aws.region'] 
         else:
             self.region = internal_conf['cloud.aws.region']
         
         if config['aws.secret_key'] and config['aws.access_key']:
-            self.ec2_resource = boto3.resource('ec2', 
+            self.ec2_resource: boto3.session.Session.resource = boto3.resource('ec2',
                                                 aws_access_key_id=config['aws.access_key'],
                                                 aws_secret_access_key=config['aws.secrey_key'], 
                                                 region_name=self.region)
-
+            self.ec2_client = self.ec2_resource.meta.client
 
             
         else:
             # This will look for the env variables
-            self.ec2_resource=boto3.client('ec2')
+            self.ec2_client: boto3.botocore.client.BaseClient = boto3.client('ec2', region=self.region)
 
 
-        self.ec2_client = ec2_resource.meta.client
-        self.aws_instances = List[Dict[str, Any]] = []
-        self.message_queue: AwsInstance = []
+        self.aws_instances: List[Dict[str, Any]] = []
+        self.message_queue: AwsInstance = {}
         self.security_group_id: str = ''
         self.worker_count: int = config['cloud.aws.worker_count']
+        self.default_message_queue_ami = internal_conf['cloud.aws.message_queue_ami']
+        self.default_worker_ami = internal_conf['cloud.aws.worker_ami']
+        self.console_output = context.console_output
 
-    def _provision_instances(self, timeout_ms, count=2, **kwargs) -> List[str]:
-        """Start and configure instances"""
-
-        # MessageQueue
+    def __provision_instances_with_timeout(self, count, **kwargs) -> None:
         imageId = kwargs.get('imageId', default=self.default_message_queue_ami)
-        instanceType = 'm4.large' # TODO pull from config
+        instanceType = 'm4.large'  # TODO pull from config
         self.message_queue = self.ec2_client.create_instances(ImageId=imageId,
                                                               MinCount=1,
                                                               MaxCount=1,
@@ -74,11 +78,18 @@ class AWSTools(InstanceManager):
         instanceType = kwargs.get('instanceType', default='t2.micro')
 
         self.instances = self.ec2_client.create_instances(
-                ImageId=imageId,
-                MinCount=count,
-                MaxCount=count,
-                InstanceType=instanceType,
-                SecurityGroupIds=[self.security_group_id]))
+            ImageId=imageId,
+            MinCount=count,
+            MaxCount=count,
+            InstanceType=instanceType,
+            SecurityGroupIds=[self.security_group_id])
+
+    def _provision_instances(self, timeout_ms, count=2, **kwargs) -> List[str]:
+        """Start and configure instances"""
+
+        # MessageQueue
+        pass
+        
 
     def _get_running_instances(self) -> List[Instance]:
         """Returns the currently managed instances"""
@@ -87,39 +98,40 @@ class AWSTools(InstanceManager):
     def _terminate_instances(self, timeout_ms) -> None:
         """Terminate all instances managed by AWSTools"""
         instance_ids = [instance.id for instance in self.instances]
-        self.ec2.instances.filter(InstanceIds=instance_ids).terminate()
+        self.ec2_client.instances.filter(InstanceIds=instance_ids).terminate()
 
     def __enter__(self) -> None:
         """On enter, AWSTools prepares the AWS security group and spins up the required intances"""
-        self._create_sec_group()
+        self._create_sec_group('csaopt_' + random_str(10))
 
-        # TODO: put all required parameters into kwargs
+        # TODO: put all required parameters into kwarg
         self._provision_instances(count=self.worker_count)
         self._wait_for_instances()
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
         """On exit, AWSTools terminates the started instances and removes security groups"""
         self._terminate_instances()
+        self._remove_sec_group()
     
-    def _remove_sec_group(self) -> None:
+    def _remove_sec_group(self, group_id: str) -> None:
         """Removes the security group created by CSAOpt"""
 
-        if self.security_group_id is not None:
+        if group_id is not None:
             try:
                 self.ec2.delete_security_group(GroupId=self.security_group_id)
-                logger.debug('Security Group Deleted')
+                logger.debug('Security group [{}] deleted'.format(group_id))
             except ClientError as e:
                 logger.error('Could not remove security group: {}'.format(e))
         else:
-            logger.warn('Cannot remove security group, because none was created. Skipping')
+            logger.warn('Cannot remove security group, because none was created. Skipping...')
 
-    def _create_sec_group(self, name) -> str:
+    def _create_sec_group(self, name: str) -> str:
         self.own_external_ip = get_own_ip()
-        response = self.ec2.describe_vpcs()
+        response = self.ec2_client.describe_vpcs()
         vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
 
         try:
-            response = self.ec2.create_security_group(
+            response = self.ec2_client.create_security_group(
                 GroupName=name,
                 Description='Security Group for CSAOpt',
                 VpcId=vpc_id)
@@ -143,5 +155,8 @@ class AWSTools(InstanceManager):
                 self.own_external_ip,
                 data
                 ))
+            
+            return data.GroupId
         except ClientError as e:
             logger.error('Could not create Security Group: {}', e)
+            return None
