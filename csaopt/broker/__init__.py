ef01cd34a23877ebc235e6a59f41499425dd9992
David Sere <dave@d53dev.net>
2018-08-21 10:48:02 +0200

    Added ping method and _use_stub_broker()

--- csaopt/broker/__init__.py
+++ csaopt/broker/__init__.py
@@ -4,6 +4,7 @@ import msgpack
 import msgpack_numpy
 import uuid
 import logging
+import sys
 
 from dramatiq.brokers.redis import RedisBroker
 from dramatiq.brokers.stub import StubBroker
@@ -23,6 +24,10 @@ msgpack_numpy.patch()
 log = logging.getLogger(__name__)
 
 
+def _use_stub_broker() -> bool:
+    return os.environ.get('USE_STUB_BROKER') is True
+
+
 class WorkerCommand(Enum):
     DeployModel = 'deploy_model'
     RunOptimization = 'run_optimization'
@@ -39,41 +44,62 @@ class _MsgPackEncoder(dramatiq.Encoder):
 
 class Broker():
     def __init__(self,
-                 redis_host: str='localhost',
-                 redis_port: int=6379,
-                 redis_password: str=None,
+                 host: str='localhost',
+                 port: int=6379,
+                 password: str=None,
                  queue_ids: List[str]=[]) -> None:
 
-        log.warn('Constructing {} without queue_ids'.format(Broker))
+        if len(queue_ids) < 1:
+            log.warn('Constructing {} without queue_ids'.format(Broker))
 
-        if is_pytest_run():
+        if is_pytest_run() and _use_stub_broker():
             broker = StubBroker
             broker.emit_after('process_boot')
         else:
             self.dramatiq_broker = broker = RedisBroker(
-                host=redis_host, port=redis_password, password=redis_password)
+                host=host, port=port, password=password)
 
             msgpack_encoder = _MsgPackEncoder()
-            self.result_backend = backend = RedisBackend(encoder=msgpack_encoder)
+            self.result_backend = backend = RedisBackend(
+                encoder=msgpack_encoder, client=self.dramatiq_broker.client)
             broker.add_middleware(Results(backend=backend))
 
-            dramatiq.set_broker(broker)
-            dramatiq.set_encoder(msgpack_encoder)
+        dramatiq.set_broker(broker)
+        dramatiq.set_encoder(msgpack_encoder)
 
         self.queue_ids: SortedSet = SortedSet(queue_ids)
-        self.queue_messages: Dict[str, List[dramatiq.Message]] = defaultdict(list)
+        self.queue_messages: Dict[str,
+                                  List[dramatiq.Message]] = defaultdict(list)
+
+    def ping(self, queue: Union[str, int]) -> bool:
+        queue_id = self.__extract_queue_id(queue)
+
+        msg = self.dramatiq_broker.enqueue(Message(
+            queue_name=queue_id,
+            actor_name='PingActor',
+            args=(), kwargs={},
+            options={},
+        ))
+
+        # TODO refactor for dynamic timeout
+        result = msg.get_result(
+            backend=self.result_backend, block=True, timeout=2000)
+
+        return result == 'pong'
 
     def get_queue_results(self, queue: Union[str, int], timeout_ms=5000) -> List[Dict[str, Any]]:
         queue_id: str = self.__extract_queue_id(queue)
         results = []
         for msg in self.queue_messages[queue_id]:
-            results.append(msg.get_result(self.result_backend, block=True, timeout=timeout_ms))
+            results.append(msg.get_result(backend=self.result_backend,
+                                          block=True, timeout=timeout_ms))
         return results
 
     def get_all_results(self, timeout_ms=5000) -> Dict[str, List[Dict[str, Any]]]:
         results = {}
         for queue_id in self.queue_ids:
-            results[queue_id] = self.get_queue_results(queue_id, timeout_ms=timeout_ms)
+            results[queue_id] = self.get_queue_results(
+                queue_id, timeout_ms=timeout_ms)
         return results
 
     def broadcast(self, command: WorkerCommand, payload: Dict[str, Any]):
@@ -85,8 +111,8 @@ class Broker():
 
         msg = self.dramatiq_broker.enqueue(Message(
             queue_name=queue_id,
-            actor_name='WorkerActor',
-            args=(command, payload), kwargs={},
+            actor_name='OptimizationActor',
+            args=(command.value, payload), kwargs={},
             options={},
         ))
         self.queue_messages[queue_id].append(msg)
