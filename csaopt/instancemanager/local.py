bf4fc537453a9aac17a9b5e07496c29ffabd99af
David Sere <dave@d53dev.net>
2018-09-17 22:14:36 +0200

    Refactor docker instancemanager for configurable queue and worker tags

--- csaopt/instancemanager/local.py
+++ csaopt/instancemanager/local.py
@@ -1,11 +1,10 @@
-
 import logging
 import uuid
 import time
 import sys
 
 from pyhocon import ConfigTree
-from typing import Tuple, List, Any, Dict, Union, Type
+from typing import Tuple, List, Any, Dict, Union, Type, Optional
 
 from .instancemanager import InstanceManager
 from . import Instance
@@ -14,7 +13,7 @@ from ..utils import get_free_tcp_port, random_str
 log = logging.getLogger()
 
 
-def _map_docker_to_instance(container, port=-1, is_broker: bool=False) -> Instance:
+def _map_docker_to_instance(container, port=-1, is_broker: bool = False) -> Instance:
     return Instance(str(container.name), '127.0.0.1', port=port, is_broker=is_broker, **container.labels)
 
 
@@ -29,27 +28,31 @@ except ImportError:
 class Local(InstanceManager[DockerContainerT]):
     def __init__(self, conf: ConfigTree, internal_conf: ConfigTree) -> None:
         if not docker_available:
-            raise AssertionError(
-                'Trying to instantiate Local InstanceManager, but docker-py is not available.')
+            raise AssertionError('Trying to instantiate Local InstanceManager, but docker-py is not available.')
         self.docker_client = docker.from_env()
-        self.broker: DockerContainerT = None
-        self.worker: DockerContainerT = None
+
+        # Because of the dynamic type variable, this fails to typecheck
+        # since DockerContainerT will be typed to Type[Any]
+        self.broker: DockerContainerT = ...  # type: ignore
+        self.worker: DockerContainerT = ...  # type: ignore
 
         self.run_id = run_id = random_str(8)
+
+        self.broker_docker_tag = internal_conf['cloud.local.broker_image']
+        self.worker_docker_tag = internal_conf['cloud.local.worker_image']
+
         self.broker_container_name = 'CSAOpt-Broker-' + run_id
         self.worker_container_name = 'CSAOpt-Worker-' + run_id
-        self.broker_port: int = get_free_tcp_port()
+        self.broker_port: Optional[int] = get_free_tcp_port()
         self.debug_on_cpu = conf.get('debug.gpu_simulator')
 
     def _provision_instances(self, timeout_ms, count=2, **kwargs) -> Tuple[DockerContainerT, List[DockerContainerT]]:
         self.broker = self.docker_client.containers.run(
-            'bitnami/redis',
+            self.broker_docker_tag,
             ports={'6379/tcp': kwargs['HOST_REDIS_PORT']},
             detach=True,
             # network=self.docker_network.name,
-            environment={
-                'ALLOW_EMPTY_PASSWORD': 'yes'
-            },
+            environment={'ALLOW_EMPTY_PASSWORD': 'yes'},
             name=self.broker_container_name)
 
         broker_ip = ''
@@ -59,11 +62,12 @@ class Local(InstanceManager[DockerContainerT]):
             self.broker.reload()
             broker_ip = self.broker.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
 
-        time.sleep(1.5)
+        # Sleeping here to give Redis a chance to start up
+        time.sleep(3)
         kwargs['REDIS_HOST'] = broker_ip
 
         self.worker = self.docker_client.containers.run(
-            'd53dave/csaopt-worker',
+            self.worker_docker_tag,
             detach=True,
             # network=self.docker_network.name,
             environment=kwargs,
@@ -86,11 +90,8 @@ class Local(InstanceManager[DockerContainerT]):
     def get_running_instances(self) -> Tuple[Instance, List[Instance]]:
         """Returns the currently managed instances"""
         broker, worker = self.__refresh_containers()
-        return (
-            _map_docker_to_instance(
-                broker, port=self.broker_port, is_broker=True),
-            [_map_docker_to_instance(worker)]
-        )
+        return (_map_docker_to_instance(broker, port=self.broker_port, is_broker=True),
+                [_map_docker_to_instance(worker)])
 
     def _terminate_instances(self, timeout_ms) -> None:
         self.worker.kill()
@@ -115,30 +116,22 @@ class Local(InstanceManager[DockerContainerT]):
             if self.debug_on_cpu:
                 env['NUMBA_ENABLE_CUDASIM'] = '1'
 
-            self.docker_network = self.docker_client.networks.create(
-                name='CSAOpt' + self.run_id)
+            self.docker_network = self.docker_client.networks.create(name='CSAOpt' + self.run_id)
 
-            self.broker, workers = self._provision_instances(
-                timeout_ms=10000, **env)
+            self.broker, workers = self._provision_instances(timeout_ms=10000, **env)
             self.worker = workers[0]
 
             return self
         except Exception as e:
-            log.exception(
-                'An exception occured while starting docker containers')
-            raise SystemError(
-                'An exception occured while starting docker containers: {}'.format(repr(e)))
+            log.exception('An exception occured while starting docker containers')
+            raise SystemError('An exception occured while starting docker containers: {}'.format(repr(e)))
 
     def __exit__(self, exc_type, exc_value, traceback) -> bool:
         try:
-            # TODO replace warn with debug
-            log.warn('Broker logs: \n' +
-                     self.broker.logs().decode('utf-8'))
-            log.warn('Worker logs: \n' +
-                     self.worker.logs().decode('utf-8'))
+            log.debug('Broker logs: \n' + self.broker.logs().decode('utf-8'))
+            log.debug('Worker logs: \n' + self.worker.logs().decode('utf-8'))
             self._terminate_instances(timeout_ms=10000)
             self.docker_network.remove()
         except Exception as e:
-            log.warn(
-                'An exception occured while killing docker containers: ' + str(e))
+            log.warn('An exception occured while killing docker containers: ' + str(e))
         return False
