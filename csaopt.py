f54a5552f39546ae7a568eca9bea075813309c7d
David Sere <dave@d53dev.net>
2018-09-27 22:36:11 +0200

    Removed defaults for model and config

--- csaopt.py
+++ csaopt.py
@@ -7,8 +7,8 @@ from csaopt import Runner
 from csaopt import __appname__ as csaopt_name
 from csaopt import __version__ as csaopt_version
 
-
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -26,42 +26,42 @@ def cli(ctx):
         sys.exit(1)
 
 
-@cli.command(name='run',
-             help='Run the optimization based on the provided config and model.')
-@click.option('--model',
-              default='user_model/',
-              type=click.Path(exists=True, resolve_path=True),
-              help='Folder containing the model that should be used for optimization.')
-@click.option('--conf',
-              default='conf/csaopt.conf',
-              type=click.Path(exists=True, resolve_path=True),
-              help='Path to the CSAOpt config. If not provided, \'conf/csaopt.conf\' will be used')
+@cli.command(name='run', help='Run the optimization based on the provided config and model.')
+@click.option(
+    '--model',
+    type=click.Path(exists=True, resolve_path=True),
+    help='Folder containing the model that should be used for optimization.')
+@click.option(
+    '--conf',
+    type=click.Path(exists=True, resolve_path=True),
+    help='Path to the CSAOpt config. If not provided, \'conf/csaopt.conf\' will be used')
 @click.pass_context
 def run_opt(ctx, model, conf):
-    runner = Runner(model, conf, ctx.obj)
+    runner = Runner([model], [conf], ctx.obj)
     runner.run()
     runner.console_printer.print_magenta('Bye.\n')
 
 
-@cli.command(name='check',
-             help='Check and validate the provided configuration and model.')
-@click.option('--model',
-              type=click.Path(exists=True, resolve_path=True),
-              help='Folder containing the model that should be used for optimization.')
-@click.option('--conf',
-              default='conf/csaopt.conf',
-              type=click.Path(exists=True, resolve_path=True),
-              help='Path to the CSAOpt config. If not provided, \'conf/csaopt.conf\' will be used')
-@click.option('--with-aws',
-              is_flag=True,
-              default=False,
-              help='If enabled, the check will also spin up EC2 instances to verify configuration and communication.')
+@cli.command(name='check', help='Check and validate the provided configuration and model.')
+@click.option(
+    '--model',
+    type=click.Path(exists=True, resolve_path=True),
+    help='Folder containing the model that should be used for optimization.')
+@click.option(
+    '--conf',
+    default='conf/csaopt.conf',
+    type=click.Path(exists=True, resolve_path=True),
+    help='Path to the CSAOpt config. If not provided, \'conf/csaopt.conf\' will be used')
+@click.option(
+    '--with-aws',
+    is_flag=True,
+    default=False,
+    help='If enabled, the check will also spin up EC2 instances to verify configuration and communication.')
 def run_check(**kwargs):
     print('Check called')
 
 
-@cli.command(name='cleanup',
-             help='Clean up generated files and terminate any running EC2 instances')
+@cli.command(name='cleanup', help='Clean up generated files and terminate any running EC2 instances')
 def cleanup():
     pass
 
