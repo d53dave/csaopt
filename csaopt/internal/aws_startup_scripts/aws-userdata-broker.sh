ff034ca541c4a94db7952e0caa2b7d88f2d9480c
David Sere <dave@d53dev.net>
2018-10-04 07:05:08 +0200

    Bump broker ami, fix broker userdata script typo

--- csaopt/internal/aws_startup_scripts/aws-userdata-broker.sh
+++ csaopt/internal/aws_startup_scripts/aws-userdata-broker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-run -d -p $external_port:6379 REDIS_PASSWORD=$redis_password bitnami/redis
+docker run -d -p $external_port:6379 -e REDIS_PASSWORD=$redis_password bitnami/redis
