6658939d46f76e72db2f51232d0cb31564266fad
David Sere <dave@d53dev.net>
2019-06-06 21:31:05 +0200

    Fix formatting

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 """Packaging settings."""
 
-
 from codecs import open
 from os.path import abspath, dirname, join
 from subprocess import call
@@ -8,7 +7,7 @@ from subprocess import call
 from setuptools import Command, find_packages, setup
 
 from lib import __version__
-
+from typing import List
 
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
@@ -18,7 +17,7 @@ with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
 class RunTests(Command):
     """Run all tests."""
     description = 'run tests'
-    user_options = []
+    user_options: List[str] = []
 
     def initialize_options(self):
         pass
@@ -33,15 +32,15 @@ class RunTests(Command):
 
 
 setup(
-    name = 'CSAOpt',
-    version = __version__,
-    description = 'Cloud based simulated annealing optimization framework',
-    long_description = long_description,
-    url = 'https://github.com/d53dave/csaopt',
-    author = 'David Sere',
-    author_email = 'dave@d53dev.net',
-    license = 'UNLICENSE',
-    classifiers = [
+    name='CSAOpt',
+    version=__version__,
+    description='Cloud based simulated annealing optimization framework',
+    long_description=long_description,
+    url='https://github.com/d53dave/csaopt',
+    author='David Sere',
+    author_email='dave@d53dev.net',
+    license='UNLICENSE',
+    classifiers=[
         'Intended Audience :: Developers, Scientists',
         'Topic :: Scientific',
         'License :: MIT',
@@ -49,16 +48,16 @@ setup(
         'Operating System :: GNU/Linux',
         'Programming Language :: Python :: 3.6',
     ],
-    keywords = 'cli',
-    packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['docopt'],
-    extras_require = {
+    keywords='cli',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['docopt'],
+    extras_require={
         'test': ['coverage', 'pytest', 'pytest-cov'],
     },
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'csaopt=csaopt.cli:main',
         ],
     },
-    cmdclass = {'test': RunTests},
+    cmdclass={'test': RunTests},
 )
