6a664b7407a0f004ed2b1010ff43922c6e638533
David Sere <dave@d53dev.net>
2019-06-07 21:57:12 +0200

    Add example optimization for rastrigin's function

---
+++ examples/rastrigin/rastrigin_opt.py
@@ -0,0 +1,90 @@
+# Rastrigin Function
+# https://www.sfu.ca/~ssurjano/rastr.html
+#
+# Dimensions: d
+#
+# The Rastrigin function has several local minima. It is highly multimodal, but locations of the minima are regularly
+# distributed.
+#
+# Input Domain:
+# The function is usually evaluated on the hypercube xi in [-5.12, 5.12], for all i = 1, ..., d.
+#
+# Global Minimum:
+# f(x*) = 0, at (0, 0, 0, 0)
+#
+# References:
+# Global Optimization Test Problems. Retrieved June 2013, from
+# http://www-optima.amp.i.kyoto-u.ac.jp/member/student/hedar/Hedar_files/TestGO.htm.
+
+# Pohlheim, H. GEATbx Examples: Examples of Objective Functions (2005). Retrieved June 2013, from http://www.geatbx.com/download/GEATbx_ObjFunExpl_v37.pdf.
+
+import math
+
+from csaopt.model import RandomDistribution, Precision
+from csaopt.utils import clamp
+from typing import MutableSequence, Sequence, Any, Tuple
+from math import pi
+
+# Configuration
+
+
+def distribution() -> RandomDistribution:
+    return RandomDistribution.Uniform
+
+
+def precision() -> Precision:
+    return Precision.Float64
+
+
+def dimensions() -> int:
+    return 10
+
+
+def empty_state() -> Tuple:
+    return (
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    )
+
+
+# Functions
+
+
+def cool(initial_temp: float, old_temp: float, step: int) -> float:
+    return initial_temp * math.pow(0.97, step)
+
+
+def acceptance_func(e_old: float, e_new: float, temp: float, rnd: float) -> float:
+    # prevent math.exp from under or overflowing, we can anyway constrain 0 < e^x <= (e^0 == 1)
+    x = clamp(-80, (e_old - e_new) / temp, 0.1)
+    return math.exp(x) > rnd
+
+
+def initialize(state: MutableSequence, randoms: Sequence[float]) -> None:
+    generate_next(state, state, randoms, 0)  # just delegate to generate_next
+    return
+
+
+def evaluate(state: Sequence) -> float:
+    d = len(state)
+
+    t1 = 0.0
+    for i in range(d):
+        x_i = state[i]
+        t1 += x_i * x_i - 10 * math.cos(2 * pi * x_i)
+
+    return 10 * d + t1
+
+
+def generate_next(state: Sequence, new_state: MutableSequence, randoms: Sequence[float], step) -> Any:
+    for i in range(len(state)):
+        new_state[i] = clamp(-5.12, 10.24 * (randoms[i] - 0.5), 5.12)
+    return
